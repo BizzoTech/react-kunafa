501b535ef1229f2d72b5358f2376bc3ab91fa179
Emad Shaaban <emad.shaaban92@gmail.com>
2018-01-08 10:36:55 +0200

    remove external libs from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,30 @@ module.exports = {
       ? 'js/react-kunafa.bundle.js'
       : 'js/react-kunafa.bundle.min.js'
   },
-  externals: {
-    "i18n-js": "i18n-js",
-    react: "react",
-    "react-dom": "react-dom",
-    redux: "redux",
-    "react-redux": "react-redux",
-    "kunafa-client": "kunafa-client",
-    'react-dom/server': 'react-dom/server',
-    'pouchdb': 'pouchdb'
+  // externals: {
+  //   "i18n-js": "i18n-js",
+  //   react: "react",
+  //   "react-dom": "react-dom",
+  //   redux: "redux",
+  //   "react-redux": "react-redux",
+  //   "kunafa-client": "kunafa-client",
+  //   'react-dom/server': 'react-dom/server',
+  //   'pouchdb': 'pouchdb'
+  // },
+  externals: function(context, request, callback) {
+    // Absolute & Relative paths are not externals
+    if (request.match(/^(\.{0,2})\//)) {
+      return callback();
+    }
+
+    try {
+      // Attempt to resolve the module via Node
+      require.resolve(request);
+      callback(null, request);
+    } catch(e) {
+      // Node couldn't find it, so it must be user-aliased
+      callback();
+    }
   },
   plugins: [],
   module: {
